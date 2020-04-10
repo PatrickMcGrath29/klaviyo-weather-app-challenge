ee03636a5ccd4ce3882e4b7cafe7606834ce02d1
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-04-10 16:13:43 -0400

    Adding initial client

---
+++ client/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <el-card>
+      <el-form>
+        <el-form-item label="Email Address">
+          <el-input v-model="formData.emailAddress"></el-input>
+        </el-form-item>
+        <el-form-item label="Location">
+          <el-input v-model="formData.location"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="signUp">Sign Me Up</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        emailAddress: null,
+        location: null
+      }
+    }
+  }
+}
+</script>

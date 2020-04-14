a58e38fa7d038602a2cf61ac5a6a9001d0225f49
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-04-14 14:50:46 -0400

    Adding initial code for lambda

---
+++ server/lambda.py
@@ -0,0 +1,46 @@
+import boto3
+import json
+import re
+
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('klaviyo-user-data')
+
+
+def lambda_handler(event, context):
+    response = write_entry(event)
+
+    if response:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': 'Invalid Input'
+        }
+
+
+
+def write_entry(unsafe_data):
+    validated_data = validate_data(unsafe_data)
+
+    if validated_data:
+        return table.put_item(Item=validated_data)
+
+    return False
+
+
+def validate_data(data):
+    if 'email_address' in data and 'location' in data:
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
+        if (re.search(regex, data['email_address'])):
+            return {
+                'email_address': data['email_address'],
+                'location': data['location'],
+                'email-location': data['email_address'] + "-" + data['location']
+            }
+
+    return False
